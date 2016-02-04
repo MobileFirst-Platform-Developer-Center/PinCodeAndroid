fa3d0a581aa13744151ddd40e32f3813fbbc2dbb
Lior Burg <liorbu@il.ibm.com>
2016-02-04 17:18:17 +0200

    Added resource request

--- app/src/main/java/sample/com/pincodeandroid/MainActivity.java
+++ app/src/main/java/sample/com/pincodeandroid/MainActivity.java
@@ -2,6 +2,18 @@ package sample.com.pincodeandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.worklight.wlclient.api.WLClient;
+import com.worklight.wlclient.api.WLFailResponse;
+import com.worklight.wlclient.api.WLResourceRequest;
+import com.worklight.wlclient.api.WLResponse;
+import com.worklight.wlclient.api.WLResponseListener;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WLClient.createInstance(this);
+
+        final Button getBalanceBtn = (Button) findViewById(R.id.getBalance);
+
+        getBalanceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                URI adapterPath = null;
+                try {
+                    adapterPath = new URI("/adapters/ResourceAdapter/balance");
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+                WLResourceRequest request = new WLResourceRequest(adapterPath, WLResourceRequest.GET);
+                request.send(new WLResponseListener() {
+                    @Override
+                    public void onSuccess(WLResponse wlResponse) {
+                        Log.d("Success", wlResponse.getResponseText());
+                    }
+
+                    @Override
+                    public void onFailure(WLFailResponse wlFailResponse) {
+                        Log.d("Failure", wlFailResponse.getErrorMsg());
+                    }
+                });
+            }
+        });
     }
 }
